9b1451880eec6f9236dfd29e1b80dbae20ba9148
cuongbd <bdcuongvn83git@gmail.com>
2025-04-03 17:54:41 +1100

    update Database read Mongo from process.env of vercel Signed-off-by: cuongbd <bdcuongvn83git@gmail.com>

--- database.js
+++ database.js
@@ -1,8 +1,10 @@
 import mongoose from "mongoose";
 
 //const MONGO_URI = "mongodb://localhost:27017/mydatabase"; // Thay "mydatabase" bằng tên database của bạn
-const MONGO_URI =
-  "mongodb+srv://bdcuongvn83:Ait123456@cluster0.6hlqiuj.mongodb.net/mydatabase?retryWrites=true&w=majority";
+// const MONGO_URI =
+//   "mongodb+srv://bdcuongvn83:Ait123456@cluster0.6hlqiuj.mongodb.net/mydatabase?retryWrites=true&w=majority";
+
+const MONGO_URI = process.env.MONGO_URI;
 const connectDB = async () => {
   try {
     await mongoose.connect(MONGO_URI, {
